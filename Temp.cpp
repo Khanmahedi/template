569d1ea1874244820f2d743bfc5411163d7fc271
Khanmahedi <71942063+Khanmahedi@users.noreply.github.com>
2023-02-11 00:19:12 +0600

    Create Temp.cpp

---
+++ Temp.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#pragma GCC target("popcnt")
+using namespace std;
+#define ll       long long
+#define pb      push_back
+#define TT ll time;        cin >> time; while(time--)
+#define FastIo  ios_base::sync_with_stdio(false);cin.tie(NULL) ;
+const ll int INF = 5e18 , nmax = 2501 , block = 350 , N = 2e6+20 , mod = 1e9+7;
+#ifndef ONLINE_JUDGE
+#include "algodebug.h"
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+
+
+int main(){
+ 
+
+}  
